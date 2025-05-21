a8accab39fa4b0c214cb29e15ee320ecde884470
olayinka sanyaolu <yellowbounty55@gmail.com>
2025-05-21 12:51:34 +0100

    Add script to print 'C is fun' x times or error message

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,9 +1,9 @@
 const arg = process.argv[2];
 const num = Number(arg);
 
-if (!Number.isInteger(num) || num <= 0) {
+if (isNaN(num)) {
   console.log('Missing number of occurrences');
-} else {
+} else if (num > 0 && Number.isInteger(num)) {
   let i = 0;
   let output = '';
   while (i < num) {
@@ -12,3 +12,4 @@ if (!Number.isInteger(num) || num <= 0) {
   }
   console.log(output);
 }
+// For zero or negative numbers, do nothing (no output)
